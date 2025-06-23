fa499d5c9f08ad068f90a1adcc44db899a173644
Philipp <philipp@bitfactory.io>
2025-06-23 23:44:42 +0200

    remove

--- src/app/components/film-card/film-card.ts
+++ src/app/components/film-card/film-card.ts
@@ -7,8 +7,6 @@ import { Component, input } from '@angular/core';
     <div class="card" style="height: 100%;">
       <div class="card-body">
         <h5 class="card-title">{{this.title()}}</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">by George Lucas, 25 May 1977</h6>
-
         <p class="card-text">{{this.description()}}</p>
       </div>
     </div>
